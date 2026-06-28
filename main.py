02e9b18e35fbd7eed442c6d648be82d5850ed706
Rafael Campos <rafaelcamppos13@gmail.com>
2026-06-28 08:22:26 -0300

    Sprint 1 concluida

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from openpyxl import Workbook
+from openpyxl.styles import Font, PatternFill, Alignment
 
 BASE = Path(__file__).parent
 EXCEL = BASE / "excel"
@@ -9,13 +10,42 @@ EXCEL.mkdir(exist_ok=True)
 arquivo = EXCEL / "FINANCE_CONTROL_PRO.xlsx"
 
 wb = Workbook()
-
 ws = wb.active
+
 ws.title = "Dashboard"
 
-ws["A1"] = "FINANCE CONTROL PRO"
-ws["A3"] = "Sistema inicial criado com sucesso"
+# largura
+ws.column_dimensions["A"].width = 40
+
+# titulo
+ws.merge_cells("A1:F3")
+ws.row_dimensions[1].height = 30
+ws.row_dimensions[2].height = 30
+ws.row_dimensions[3].height = 15
+
+titulo = ws["A1"]
+
+titulo.value = "FINANCE CONTROL PRO"
+
+titulo.font = Font(
+    size=18,
+    bold=True,
+    color="FFFFFF"
+)
+
+titulo.fill = PatternFill(
+    fill_type="solid",
+    start_color="0F172A"
+)
+
+titulo.alignment = Alignment(
+    horizontal="center",
+    vertical="center"
+)
+
+# mensagem
+ws["A4"] = "Sistema inicial criado com sucesso"
 
 wb.save(arquivo)
 
-print("Projeto criado:", arquivo)
+print("Dashboard estilizado criado")
